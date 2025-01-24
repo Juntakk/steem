a7cc0e6b0fbc0f4359be51647df92c5f07b5dc02
Nicolas Gauthier <nick.habashi@gmail.com>
2025-01-24 12:02:47 -0500

    wishlist persistance

--- src/App.jsx
+++ src/App.jsx
@@ -11,33 +11,18 @@ import { WishList } from "./components/content/WishList";
 import { useAuth } from "./contexts/authContext";
 import data from "../src/data/data.js";
 import "./index.css";
-// import axios from "axios";
 
 function App() {
-  const [wishList, setWishList] = useState([]);
-  const [wishedItems, setWishedItems] = useState(0);
-  const [games, setGames] = useState(data); // Initialize directly from data.js
-  const { isLoggedIn } = useAuth();
+  const [wishList, setWishList] = useState(
+    JSON.parse(localStorage.getItem("wishlist"))
+  );
+  const [wishedItems, setWishedItems] = useState(
+    parseInt(localStorage.getItem("wishedItems"))
+  );
   const [isWishListed, setIsWishListed] = useState(false);
-  // const [isLoading, setIsLoading] = useState(true);
+  const [games, setGames] = useState(data);
+  const { isLoggedIn } = useAuth();
   const navigate = useNavigate();
-  // const location = useLocation();
-
-  //WishList storage
-  useEffect(() => {
-    const storedWishList = JSON.parse(localStorage.getItem("wishlist"));
-    const storedWishedItems = JSON.parse(localStorage.getItem("wishedItems"));
-    if (storedWishList) {
-      setWishList(storedWishList);
-    }
-    if (storedWishedItems) {
-      setWishedItems(storedWishedItems);
-    }
-  }, []);
-  useEffect(() => {
-    localStorage.setItem("wishlist", JSON.stringify(wishList));
-    localStorage.setItem("wishedItems", JSON.stringify(wishedItems));
-  }, [wishList, wishedItems]);
 
   const addToWishList = (game) => {
     const gameIds = wishList.map((item) => item._id);
@@ -51,13 +36,25 @@ function App() {
       setWishList([...wishList, game]);
       setWishedItems((prev) => prev + 1);
       navigate("/wishlist");
+      localStorage.setItem("wishlist", JSON.stringify([...wishList, game]));
+      localStorage.setItem(
+        "wishedItems",
+        parseInt(JSON.stringify(wishedItems + 1))
+      );
     } else {
       setIsWishListed(true);
     }
   };
+
   const removeFromWishList = (id) => {
-    setWishList(wishList.filter((game) => game._id !== id));
+    const updatedWishList = wishList.filter((game) => game._id !== id);
+    setWishList(updatedWishList);
     if (wishedItems > 0) {
+      localStorage.setItem(
+        "wishedItems",
+        parseInt(JSON.stringify(wishedItems - 1))
+      );
+      localStorage.setItem("wishlist", JSON.stringify(updatedWishList));
       setWishedItems(wishedItems - 1);
     } else {
       setIsWishListed(false, id);
--- src/components/static/NotFound.jsx
+++ src/components/static/NotFound.jsx
@@ -1,13 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import "./styles/notFound.css";
 import React from "react";
+import image from "../../images/404.jpg";
 
 export const NotFound = () => {
   const navigate = useNavigate();
   return (
     <div className="notFoundPage">
       <h2 className="notFoundText">Page Not Found</h2>
-      <img className="notFoundImage" src="../images/404.png" alt="" />
+      <img className="notFoundImage" src={image} alt="" />
       <button className="backToStoreButton" onClick={() => navigate("/games")}>
         Back to Store
       </button>
